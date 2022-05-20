4fe2a3e2f6971ba0eea09089a7f1916b4d855160
ctestingsl <105916058+ctestingsl@users.noreply.github.com>
2022-05-20 10:26:25 +0100

    Update streamlit_app.py
    add section header and convert response to json

--- streamlit_app.py
+++ streamlit_app.py
@@ -15,7 +15,8 @@ streamlit.text(':blueberries: Omega 3 & Blueberry Oatmeal')
 
 streamlit.dataframe(fruits_to_show)
 
+streamlit.header("Fruityvice Fruit Advice!")
 
 import requests
 fruityvice_response = requests.get("https://fruityvice.com/api/fruit/watermelon")
-streamlit.text(fruityvice_response)
+streamlit.text(fruityvice_response.json())
