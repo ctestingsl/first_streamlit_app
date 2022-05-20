4b02b69e8fa2507f6fdda258bd5a4026713fa93a
ctestingsl <105916058+ctestingsl@users.noreply.github.com>
2022-05-20 10:23:11 +0100

    Update streamlit_app.py
    previous updates

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,1 +1,16 @@
 
+import streamlit
+import pandas 
+my_fruit_list = pandas.read_csv('https://uni-lab-files.s3.us-west-2.amazonaws.com/dabw/fruit_macros.txt')
+my_fruit_list = my_fruit_list.set_index('Fruit')
+
+# put a pick list here 
+fruits_selected = streamlit.multiselect('Pick some fruits:', list(my_fruit_list.index))
+fruits_to_show = my_fruit_list.loc[fruits_selected]
+
+streamlit.title('My Mom\'s New Healthy Diner')
+
+streamlit.header('Breakfast Menu')
+streamlit.text(':blueberries: Omega 3 & Blueberry Oatmeal')
+
+streamlit.dataframe(fruits_to_show)
